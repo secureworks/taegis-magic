76374ae52e9110356e249a86a856cb5406548973
Micah Pegman <mpegman@octolabs.io>
2024-01-17 21:19:32 +0000

    threat intel correlations
    Signed-off-by: Micah Pegman <mpegman@octolabs.io>

--- taegis_magic/pandas/context.py
+++ taegis_magic/pandas/context.py
@@ -1,9 +1,13 @@
 """Context gathering functions."""
 
+from typing import Callable, Dict, List, Optional
+
 import pandas as pd
-from IPython.core.display import display
-from typing import Dict, List
+import numpy as np
 import panel as pn
+from IPython.core.display import display
+from taegis_magic.pandas.alerts import inflate_raw_events
+from taegis_sdk_python import GraphQLService
 
 pn.extension("tabulator")
 
@@ -120,6 +124,9 @@ def relate_entities(df: pd.DataFrame) -> pd.DataFrame:
     ValueError
         column not found, run pipe function 'normalize_entities'
     """
+    if df.empty:
+        return df
+
     if "taegis_magic.entities.field" not in df.columns:
         raise ValueError(
             "taegis_magic.entities.field column not found, run pipe function 'normalize_entities'"
@@ -170,7 +177,13 @@ def relate_entities(df: pd.DataFrame) -> pd.DataFrame:
     return df.drop_duplicates("taegis_magic.entities.value")
 
 
-def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
+def generate_context_queries(
+    df: pd.DataFrame,
+    open_alerts_timeframe="EARLIEST=-1d",
+    resolved_alerts_timeframe="EARLIEST=-30d",
+    investigations_timeframe="EARLIEST=-30d",
+    events_timeframe="EARLIEST=-1d",
+) -> pd.DataFrame:
     """Generate Taegis Query Language queries based on logical entities.
 
     Parameters
@@ -183,6 +196,9 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         Generate Query DataFrame
     """
+
+    if df.empty:
+        return df
 
     if "taegis_magic.entities.field" not in df.columns:
         raise ValueError(
@@ -215,7 +231,7 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
             metadata.title != '{title}' AND
             status = 'OPEN' AND
             investigation_ids IS NULL
-        EARLIEST=-1d
+        {open_alerts_timeframe}
         """
 
         resolved_alerts_query = f"""
@@ -223,7 +239,7 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
         WHERE
             ({' OR '.join(query_params)}) AND
             status != 'OPEN'
-        EARLIEST=-30d | aggregate count by metadata.title, entities, status, resolution_reason
+        {resolved_alerts_timeframe} | aggregate count by metadata.title, entities, status, resolution_reason
         """
 
         investigations_query = f"""
@@ -231,13 +247,13 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
         WHERE
             ({' OR '.join(query_params)}) AND
             investigation_ids IS NOT NULL
-        EARLIEST=-30d | aggregate count by metadata.title, entities, investigation_ids, status
+        {investigations_timeframe} | aggregate count by metadata.title, entities, investigation_ids, status
         """
 
         events_query = f"""
         WHERE
             ({' OR '.join(query_params)})
-        EARLIEST=-1d
+        {events_timeframe}
         """
 
         return pd.Series(
@@ -255,8 +271,6 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
             ],
         )
 
-        return pd.DataFrame()
-
     df2 = pd.concat(
         [df, df.apply(generate_context_from_row, axis=1)], axis=1
     ).reset_index(drop=True)
@@ -265,14 +279,21 @@ def generate_context_queries(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def get_facet(df: pd.DataFrame, columns: List[str], title: str) -> pn.Card:
-    df = (
-        df.groupby(columns)
-        .size()
-        .rename("count")
-        .sort_values(ascending=False)
-        .to_frame()
-        .reset_index()
-    )
+    for _ in columns.copy():
+        try:
+            df = (
+                df.groupby(columns)
+                .size()
+                .rename("count")
+                .sort_values(ascending=False)
+                .to_frame()
+                .reset_index()
+            )
+        except (KeyError, TypeError) as exc:
+            columns.remove(exc.args[0])
+            continue
+
+        break
 
     header_filters = {
         column: {"type": "input", "func": "like", "placeholder": "Filter"}
@@ -295,7 +316,18 @@ def get_facet(df: pd.DataFrame, columns: List[str], title: str) -> pn.Card:
     return card
 
 
-def display_facets(queries: Dict[str, pd.DataFrame]):
+def display_facets(
+    queries: Dict[str, pd.DataFrame], additional_columns: Optional[List[str]] = None
+):
+    """Display Card facets for each entity.
+
+    Parameters
+    ----------
+    queries : Dict[str, pd.DataFrame]
+        The DataFrame results from the entity queries.
+    additional_columns : Optional[List[str]], optional
+        Additional columns to display on each facet (usually provided by add_threat_intel), by default None
+    """
     for entity in queries:
         if not queries[entity]["open_alerts"].empty:
             open_alerts = get_facet(
@@ -346,7 +378,7 @@ def display_facets(queries: Dict[str, pd.DataFrame]):
         for schema in schemas:
             df = queries[entity]["events"][
                 queries[entity]["events"]["resource_id"].str.contains(schema)
-            ].dropna(how="all", axis=1)
+            ].dropna(how="all", axis=1, inplace=False)
             if schema == "auth":
                 columns = [
                     "source_address",
@@ -441,6 +473,9 @@ def display_facets(queries: Dict[str, pd.DataFrame]):
             else:
                 columns = list(df.columns)
 
+            if additional_columns:
+                columns.extend(additional_columns)
+
             schema_cards.append(get_facet(df=df, columns=columns, title=schema))
 
         display(
@@ -452,3 +487,219 @@ def display_facets(queries: Dict[str, pd.DataFrame]):
                 title=entity,
             )
         )
+
+
+def add_threat_intel(
+    df: pd.DataFrame,
+    correlations: List[Callable],
+    tenant_id: Optional[str] = None,
+    region: Optional[str] = None,
+) -> pd.DataFrame:
+    """Correlate Threat Indicators to logical types.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Alerts/Events DataFrame.
+    correlations : List[Callable]
+        List of correlation functions.
+    tenant_id : Optional[str]
+        Teanant ID.
+    region : Optional[str]
+        Taegis Region.
+
+    Returns
+    -------
+    pd.DataFrame
+        Correlated DataFrame.
+
+    Raises
+    ------
+    ValueError
+        resource_id or event_data.resource_id not found in DataFrame.
+    """
+    if df.empty:
+        return df
+
+    df = df.copy()
+
+    column = ""
+    prefix = ""
+    if "event_ids" in df.columns:
+        df = df.pipe(inflate_raw_events)
+        prefix = "event_data."
+
+    column = f"{prefix}resource_id"
+
+    if column not in df.columns:
+        return df
+
+    # https://docs.ctpx.secureworks.com/search/builder/advanced_search/#logical-type-mappings
+    # we only need to define for @ip, @domain, and @hash
+    ip_field_map = {
+        "auth": [
+            "target_address",
+            "source_address",
+        ],
+        "cloudaudit": [
+            "source_address",
+        ],
+        "dnsquery": [
+            "source_address",
+            "destination_address",
+        ],
+        "http": [
+            "source_address",
+            "destination_address",
+            "true_source_address",
+        ],
+        "netflow": [
+            "source_address",
+            "destination_address",
+            "source_nat_address",
+            "destination_nat_address",
+        ],
+        "nids": [
+            "source_address",
+            "destination_address",
+        ],
+    }
+
+    domain_field_map = {
+        "auth": [
+            "target_domain_name",
+            "source_domain_name",
+            "extra_targetoutbounddomainname",
+        ],
+        "dnsquery": [
+            "query_name",
+        ],
+    }
+
+    hash_field_map = {
+        "auth": [
+            "process_file_hash",
+            "process_file_hash.md5",
+            "process_file_hash.sha1",
+            "process_file_hash.sha256",
+            "process_file_hash.sha512",
+        ],
+        "filemod": [
+            "file_hash,parent_process_file_hash.md5",
+            "parent_process_file_hash.sha1",
+            "parent_process_file_hash.sha256",
+            "parent_process_file_hash.sha512",
+            "process_file_hash.md5",
+            "process_file_hash.sha1",
+            "process_file_hash.sha256",
+            "process_file_hash.sha512",
+            "file_hash.md5",
+            "file_hash.sha1",
+            "file_hash.sha256",
+            "file_hash.sha512",
+        ],
+        "process": [
+            "program_hash.md5",
+            "program_hash.sha1",
+            "program_hash.sha256",
+            "program_hash.sha512",
+            "target_program.sha1_hash",
+            "host_program.sha1_hash",
+        ],
+    }
+
+    def parse_fields(row, field_map, prefix: str = ""):
+        for schema, field_list in field_map.items():
+            if schema in row[f"{prefix}resource_id"]:
+                l = list(
+                    set(
+                        [
+                            row[f"{prefix}{field}"]
+                            for field in field_list
+                            if f"{prefix}{field}" in row
+                        ]
+                    )
+                )
+                if np.nan in l:
+                    l.remove(np.nan)
+                return l
+        return []
+
+    df["logicals.ip"] = df.apply(
+        parse_fields, field_map=ip_field_map, prefix=prefix, axis=1
+    )
+    df["logicals.domain"] = df.apply(
+        parse_fields, field_map=domain_field_map, prefix=prefix, axis=1
+    )
+    df["logicals.hash"] = df.apply(
+        parse_fields, field_map=hash_field_map, prefix=prefix, axis=1
+    )
+
+    logical_ips = set()
+    logical_domains = set()
+    logical_hashes = set()
+
+    for _, row in df.iterrows():
+        logical_ips.update(row["logicals.ip"])
+        logical_domains.update(row["logicals.domain"])
+        logical_hashes.update(row["logicals.hash"])
+
+    indicators = logical_ips | logical_domains | logical_hashes
+
+    for callable in correlations:
+        df = df.pipe(
+            callable, indicators=indicators, tenant_id=tenant_id, region=region
+        )
+
+    return df
+
+
+def get_ti_pubs(
+    df: pd.DataFrame,
+    indicators: List[str],
+    tenant_id: Optional[str] = None,
+    region: Optional[str] = None,
+) -> pd.DataFrame:
+    """Correlate Threat Indicators to Threat Intelligence Publications.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Alerts/Events DataFrame.
+    indicators : List[str]
+        List of indicators; IPs, Domains, Hashes.
+    tenant_id : Optional[str], optional
+        Taegis Tenant ID, by default None
+    region : Optional[str], optional
+        Taegis Region, by default None
+
+    Returns
+    -------
+    DataFrame
+        Correlated DataFrame.
+    """
+    df = df.copy()
+
+    service = GraphQLService(tenant_id=tenant_id, environment=region)
+
+    # get TI Pubs
+    ti_pubs = {}
+    for indicator in indicators:
+        with service(output="id Type Name"):
+            results = service.threat.query.threat_publications(indicator)
+        ti_pubs[indicator] = results
+    # ti_pubs
+
+    # correlate
+    def check_tips(row, ti_pubs):
+        for indicator, pub_list in ti_pubs.items():
+            for col in [idx for idx in row.index if idx.startswith("logicals.")]:
+                if pub_list and indicator in row[col]:
+                    return pd.Series([True, pub_list])
+        return pd.Series([False, []])
+
+    df[["tips.found", "tips.publications"]] = df.apply(
+        check_tips, ti_pubs=ti_pubs, axis=1
+    )
+
+    return df
