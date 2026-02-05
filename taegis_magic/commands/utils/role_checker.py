adb86a1dd952975352bdf806aa4078e35fe8ce33
chris.langreo <chris.langreo@sophos.com>
2026-02-05 15:24:57 +0000

    CX_159462; Update has_role to use subjects api
    Signed-off-by: chris.langreo <chris.langreo@sophos.com>

--- taegis_magic/commands/utils/role_checker.py
+++ taegis_magic/commands/utils/role_checker.py
@@ -1,12 +1,10 @@
 from taegis_magic.core.service import get_service
 
-def has_role(role: str, region: str):
-    """Check if the current user has the specified role."""
-
-    service = get_service(environment=region)
-    user_info = service.users.query.current_tdruser()
+def has_role(role: str, region: str) -> bool:
+    """Check if the current subject has the specified role."""
+    
+    with get_service(exclude_deprecated_output=False, environment=region) as service:
+        subject_info = service.subjects.query.current_subject()
 
     role_to_check = role.lower()
-
-    return any(r.role_name.lower() == role_to_check for r in user_info.role_assignments)
-
+    return any(r.role_name.lower() == role_to_check for r in subject_info.role_assignment_data.role_assignments)
