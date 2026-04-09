64ff99a0218a5106fe77b00ba949be337ea7f77a
chris.langreo <chris.langreo@sophos.com>
2026-04-09 17:11:19 -0400

    CX-159011; Create _process_pivot_with_map
    Signed-off-by: chris.langreo <chris.langreo@sophos.com>

--- taegis_magic/pandas/process.py
+++ taegis_magic/pandas/process.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import logging
-from typing import Optional
+from typing import Mapping, Optional
 from taegis_magic.core.service import get_service
 from taegis_magic.pandas.utils import chunk_list
 from dataclasses import dataclass
@@ -22,9 +22,12 @@ PROCESS_PIPE_TEMPLATE = "process_pipe_template.jinja"
 
 NETFLOW_PIVOT_COLUMNS = ["host_id", "sensor_id", "sensor_tenant", "sensor_type", "tenant_id"]
 HTTP_PIVOT_COLUMNS = ["host_id", "process_correlation_id", "resource_id", "sensor_id", "sensor_type", "tenant_id"]
+AUTH_PIVOT_COLUMNS = ["host_id", "process_correlation_id", "resource_id", "sensor_id", "sensor_type", "tenant_id"]
 
 NETFLOW = "netflow"
 HTTP = "HTTP"
+AUTH = "AUTH"
+
 
 CONFIG = get_config()
 if not CONFIG.has_section(QUERIES_SECTION):
@@ -189,64 +192,97 @@ def process_pivot_netflow(
     *,
     region: str,
     tenant_id: str,
+    pivot_map: Optional[Mapping[str, str]] = None,
     earliest: Optional[str] = "1d"
 ) -> pd.DataFrame:
-    """Pivot aggregate process data into non-aggregate netflow event rows.
+    """Pivot aggregate process data into non-aggregate netflow event rows."""
 
-    The input DataFrame is expected to contain aggregate process data. Columns present in the 
-    DataFrame and a static pivot column list are used to build per-row sub-query filters,
-    which are then combined with OR logic and issued as a single query against netflow.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame containing aggregate process data.
-    region : str,
-        Taegis region.
-    tenant_id : str,
-        Tenant ID to query against.
-    earliest : str, optional
-        Date filter to apply when querying against netflow events. Based on Taegis Query language. A "-" will be prepended to whatever value is provided. 
-
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame of raw netflow event rows matching the aggregate filters.
-    """
-
-    return _process_pivot_base_func(df, region, tenant_id, PROCESS_PIPE_TEMPLATE, NETFLOW, NETFLOW_PIVOT_COLUMNS, earliest)
+    return _process_pivot_with_map(df, region, tenant_id, PROCESS_PIPE_TEMPLATE, NETFLOW, NETFLOW_PIVOT_COLUMNS, earliest, pivot_map)
 
 def process_pivot_http(
     df: pd.DataFrame,
     *,
     region: str,
     tenant_id: str,
+    pivot_map: Optional[Mapping[str, str]] = None,
     earliest: Optional[str] = "1d"
 ) -> pd.DataFrame:
-    """Pivot aggregate process data into non-aggregate http event rows.
+    """Pivot aggregate process data into non-aggregate http event rows."""
 
-    The input DataFrame is expected to contain aggregate process data. Columns present in the 
-    DataFrame and a static pivot column list are used to build per-row sub-query filters,
-    which are then combined with OR logic and issued as a single query against http.
+    return _process_pivot_with_map(df, region, tenant_id, PROCESS_PIPE_TEMPLATE, HTTP, HTTP_PIVOT_COLUMNS, earliest, pivot_map)
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame containing aggregate process data.
-    region : str,
-        Taegis region.
-    tenant_id : str,
-        Tenant ID to query against.
-    earliest : str, optional
-        Date filter to apply when querying against netflow events. Based on Taegis Query language. A "-" will be prepended to whatever value is provided. 
 
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame of raw netflow event rows matching the aggregate filters.
-    """
+def process_pivot_auth(
+    df: pd.DataFrame,
+    *,
+    region: str,
+    tenant_id: str,
+    pivot_map: Optional[Mapping[str, str]] = None,
+    earliest: Optional[str] = "1d"
+) -> pd.DataFrame:
+    """Pivot aggregate process data into non-aggregate http event rows."""
 
-    return _process_pivot_base_func(df, region, tenant_id, PROCESS_PIPE_TEMPLATE, HTTP, HTTP_PIVOT_COLUMNS, earliest)
+    return _process_pivot_with_map(df, region, tenant_id, PROCESS_PIPE_TEMPLATE, HTTP, HTTP_PIVOT_COLUMNS, earliest, pivot_map)
+
+
+def _execute_pivot_subqueries(
+    df: pd.DataFrame,
+    unique_sub_queries: list[str],
+    query_template: str,
+    table: str,
+    region: str,
+    tenant_id: str,
+    earliest: str,
+) -> pd.DataFrame:
+    """Helper function to execute queries for pivot functions"""
+
+    template = jinja_env.get_template(query_template)
+    service = get_service(environment=region, tenant_id=tenant_id)
+    query_options = EventQueryOptions(
+        timestamp_ascending=True,
+        page_size=1000,
+        max_rows=100000,
+        aggregation_off=True,
+    )
+
+    results = []
+
+    for chunk in chunk_list(unique_sub_queries, 100):
+
+        query = template.render(table=table, filters=chunk, earliest=f"-{earliest}")
+
+        query_result = service.events.subscription.event_query(
+            query=query,
+            options=query_options,
+            metadata={
+                "callerName": CONFIG[QUERIES_SECTION].get(
+                    "callername", fallback="Taegis Magic"
+                ),
+            },
+        )
+
+        if not query_result[0].result.rows:
+            log.debug("No results were returned from query.")
+            continue
+
+        results.extend(query_result)
+        next_page = get_next_page(query_result)
+
+        while next_page:
+            query_result = service.events.subscription.event_page(next_page)
+            results.extend(query_result)
+            next_page = get_next_page(query_result)
+
+    if not results:
+        log.info("No results were returned from query.")
+        return df
+
+    return to_dataframe(
+        row
+        for r in results
+        if r.result and r.result.rows
+        for row in r.result.rows
+    )
 
 
 def _process_pivot_base_func(
@@ -265,9 +301,11 @@ def _process_pivot_base_func(
 
     In this particular case, the input DataFrame contains aggregate process data. The function parses the input DataFrame and creates a list
     of WHERE clauses based on columns that exist in both the input DataFrame and the `pivot_columns` parameter. It will then execute a query
-    against the table to "pivot" to (i.e. the `table` parameter) and the function will return whatever is returned by that query. So if a 
+    against the table to `pivot` to (i.e. the `table` parameter) and the function will return whatever is returned by that query. So if a 
     pivot function is for process -> netflow, the input DataFrame will contains aggregate process data and what is returned is raw non-aggregate
     data from the netflow table based on the WHERE clauses. 
+
+    Please note that the values in the pivot_columns list are columns that must exist in both the process table and the table to pivot to.
 
     Parameters
     ----------
@@ -359,51 +397,154 @@ def _process_pivot_base_func(
             f"{NETFLOW_PIVOT_COLUMNS}"
         )
 
-    template = jinja_env.get_template(query_template)
-    service = get_service(environment=region, tenant_id=tenant_id)
-    query_options = EventQueryOptions(
-        timestamp_ascending=True,
-        page_size=1000,
-        max_rows=100000,
-        aggregation_off=True,
+    return _execute_pivot_subqueries(
+        df,
+        unique_sub_queries,
+        query_template,
+        table,
+        region,
+        tenant_id,
+        earliest,
     )
 
-    results = []
 
-    for chunk in chunk_list(unique_sub_queries, 100):
+def _process_pivot_with_map(
+    df: pd.DataFrame,
+    region: str,
+    tenant_id: str,
+    query_template: str,
+    table: str,
+    pivot_columns: list[str],
+    earliest: str,
+    pivot_map: Optional[Mapping[str, str]]
+) -> pd.DataFrame:
+    """Works similarly to `_process_pivot_base_func`, but optional `pivot_map` contains keys that are column names in the input
+    DataFrame and the corresponding values are column names in the table to pivot to. Therefore, the pivot_map remaps DataFrame
+    DataFrame column names to target table column names in WHERE clauses.
 
-        query = template.render(table=table, filters=chunk, earliest=f"-{earliest}")
+    When `pivot_map` is None, `_process_pivot_base_func` is called.
 
-        query_result = service.events.subscription.event_query(
-            query=query,
-            options=query_options,
-            metadata={
-                "callerName": CONFIG[QUERIES_SECTION].get(
-                    "callername", fallback="Taegis Magic"
-                ),
-            },
+    When `pivot_map` is provided, `pivot_columns` is ignored. Every key in `pivot_map` must exist as a column in the input DataFrame.
+    Each key's corresponding value should be a column that exists in the table to pivot to, i.e. the `table` parameter. Whereas in 
+    `_process_pivot_base_func` the where clauses are built using the column names and their row values directly from the input DataFrame,
+    this function builds where clauses using the column names from the `pivot_map` and the row values are still from the input DataFrame.
+
+    This function is being created to allow for additional "customization" to overcome the "limitations" of `_process_pivot_base_func`.
+    `_process_pivot_base_func` only works with a static list of columns that must exist both in the `process` table and the table to 
+    pivot to. If there is schema evolution, a mismatch in column names between `process` and target table with similar data, etc. then
+    the `_process_pivot_base_func` is not as useful. This function helps overcome said limitations without adding new code. 
+
+
+    Example
+    -------
+
+    The following example demonstrates what the input DataFrame is expected to look like, what `pivot_map` parameter may look like,
+    and then shows what kind of query the function will execute based on the contents of the input DataFrame and `pivot_map`. 
+    This is just meant for demonstration purposes, the column names may or may not be real. 
+
+    >>> # Remember, input_df contains aggregate process info. 
+    >>> input_df
+                                    host_id      sensor_type                 env  count
+    0  550e8400-e29b-41d4-a716-446655440001  ENDPOINT_SOPHOS               alpha    100
+    1  550e8400-e29b-41d4-a716-446655440002  ENDPOINT_TAEGIS                beta    200
+    2  550e8400-e29b-41d4-a716-446655440003         FIREWALL               gamma     50
+    >>> # Example value of `pivot_map` parameter
+    >>> print(pivot_map)
+    {
+        "sensor_type": "sensor_type",
+        "env": "region"
+    }
+
+    -------------------------------------------------------------------------------------------------------
+    For the `input_df` dataframe above, the generated query would look like:
+
+        FROM netflow
+        WHERE
+            (sensor_type = 'ENDPOINT_SOPHOS' AND env = 'alpha') or 
+            (sensor_type = 'ENDPOINT_TAEGIS' AND env = 'beta') or 
+            (sensor_type = 'FIREWALL' AND env = 'gamma')
+        EARLIEST=-1d
+
+    Notice how the `pivot_map` did not include `host_id` and therefore `host_id` was not included in the query. 
+
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing aggregate process data.
+    region : str,
+        Taegis region.
+    tenant_id : str,
+        Tenant ID to query against.
+    query_template: str,
+        Name of Jinja template for querying desired table.
+    table: str,
+        Table to generate a query against, to be injected into Jinja query template. 
+    pivot_columns: str,
+        A list of columns that are found in both the `process` table and the `table` to execute queries against. This list will determine 
+        the WHERE clauses that will get generated when executing the query against the `table parameter`.
+    earliest : str,
+        Date filter to apply when querying against netflow events. Based on Taegis Query language. A "-" will be prepended to whatever value is provided. 
+    pivot_map : Mapping[str, str] | None
+        Maps input DataFrame column names to target table column names for query filters.
+    """
+    
+    if pivot_map is None:
+        return _process_pivot_base_func(
+            df, region, tenant_id, query_template, table, pivot_columns, earliest
         )
 
-        if not query_result[0].result.rows:
-            log.debug("No results were returned from query.")
-            continue
-
-        results.extend(query_result)
-        next_page = get_next_page(query_result)
-
-        while next_page:
-            query_result = service.events.subscription.event_page(next_page)
-            results.extend(query_result)
-            next_page = get_next_page(query_result)
-
-    if not results:
-        log.info("No results were returned from query.")
+    if df.empty:
         return df
 
-    return to_dataframe(
-        row
-        for r in results
-        if r.result and r.result.rows
-        for row in r.result.rows
+    cols = list(pivot_map.keys())
+    if not cols:
+        log.error("pivot_map is empty; no columns to pivot on")
+        return df
+
+    missing_keys = [k for k in cols if k not in df.columns]
+    if missing_keys:
+        log.error(
+            f"DataFrame is missing columns required by pivot_map keys: {missing_keys}"
+        )
+        return df
+
+    single_quote = "'"
+    replacement = "\\'"
+
+    sub_queries = []
+    for _, row in df.iterrows():
+        row_filters = []
+        for col in cols:
+            if col not in row or pd.isna(row[col]):
+                continue
+            table_col = pivot_map[col]
+            val = row[col]
+            if not str(val).find("'") > -1:
+                row_filters.append(f"{table_col} = '{val}'")
+            else:
+                row_filters.append(
+                    f"{table_col} = e'{str(val).replace(single_quote, replacement)}'"
+                )
+        if row_filters:
+            sub_queries.append("(" + " AND ".join(row_filters) + ")")
+
+    unique_sub_queries = list(dict.fromkeys(sub_queries))
+
+    if not unique_sub_queries:
+        raise ValueError(
+            "No sub-queries could be built from the DataFrame. "
+            "Ensure the DataFrame contains non-null values in one or more of the following columns: "
+            f"{cols}"
+        )
+
+    return _execute_pivot_subqueries(
+        df,
+        unique_sub_queries,
+        query_template,
+        table,
+        region,
+        tenant_id,
+        earliest,
     )
 
